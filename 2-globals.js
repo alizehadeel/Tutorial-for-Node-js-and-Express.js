5a36d277d30f97beaebff34656d29e4cea2a853c
alizehadeel <alizehadeel12@gmail.com>
2025-02-08 08:38:00 +0500

    Added initial Node.js tutorial files.

---
+++ 2-globals.js
@@ -0,0 +1,8 @@
+//console.log(__dirname)
+//console.log(__filename)
+//console.log(require)
+//console.log(process)//module?
+
+setInterval(()=>{
+    console.log('Hello World')
+}, 1000)
---
+++ app.js
@@ -0,0 +1,8 @@
+//Node used commonJS library for modules, and everyfile in node js is modules
+//Modules-Encapsulated codes
+
+
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
